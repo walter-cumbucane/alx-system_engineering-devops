99d29d399fab96f8b5103afaaff1ca77ff20ac11
walter-cumbucane <cumbucane.walter@gmail.com>
2023-10-31 21:25:21 +0200

    Task 3 done

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -4,5 +4,5 @@ argument = ARGV[0]
 if not argument
     puts "Insert an argument to look for matching expressions"
 else
-    puts argument.scan(/hb+tn/).join
+    puts argument.scan(/hbt+n/).join
 end
